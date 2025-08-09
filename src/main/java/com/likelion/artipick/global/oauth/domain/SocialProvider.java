6e54efc3ddf3e6cc91103efb6ec35e27e9443677
KTH1007 <taeheon@gimtaeheon-ui-MacBookPro.local>
2025-08-09 19:59:29 +0900

    feat/#2: JPA Auditing 구현

---
+++ src/main/java/com/likelion/artipick/global/oauth/domain/SocialProvider.java
@@ -0,0 +1,5 @@
+package com.likelion.artipick.global.oauth.domain;
+
+public enum SocialProvider {
+    GOOGLE, KAKAO
+}
---
+++ src/main/java/com/likelion/artipick/user/domain/RoleType.java
@@ -0,0 +1,5 @@
+package com.likelion.artipick.user.domain;
+
+public enum RoleType {
+    USER, ADMIN
+}
