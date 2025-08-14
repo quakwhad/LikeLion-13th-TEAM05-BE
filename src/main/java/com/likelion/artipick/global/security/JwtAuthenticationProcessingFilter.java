ab5d285aab96548cb3940702fded750e21b2d3d3
quakwhad <quakwhad@gmail.com>
2025-08-14 19:23:40 +0900

    delete: checkAccessTokenAndAuthentication에서 로그 기능 삭제

--- src/main/java/com/likelion/artipick/global/security/JwtAuthenticationProcessingFilter.java
+++ src/main/java/com/likelion/artipick/global/security/JwtAuthenticationProcessingFilter.java
@@ -67,7 +67,6 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
 
     public void checkAccessTokenAndAuthentication(HttpServletRequest request, HttpServletResponse response,
                                                   FilterChain filterChain) throws ServletException, IOException {
-        log.info("checkAccessTokenAndAuthentication() 호출");
         jwtService.extractAccessToken(request)
                 .ifPresent(accessToken -> {
                     String email = jwtService.verifyTokenAndGetEmail(accessToken);
