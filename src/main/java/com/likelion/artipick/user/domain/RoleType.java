c0b779ef455e1a4a9d0561b0d42692293182d108
quakwhad <quakwhad@gmail.com>
2025-08-14 20:39:59 +0900

    fix: RoleType.java main과 같게 수정

--- src/main/java/com/likelion/artipick/user/domain/RoleType.java
+++ src/main/java/com/likelion/artipick/user/domain/RoleType.java
@@ -3,8 +3,6 @@ package com.likelion.artipick.user.domain;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-// 사용자 역할을 정의하는 Enum
-// 관리자, 일반 사용자, 게스트로 구분
 @Getter
 @RequiredArgsConstructor
 public enum RoleType {
