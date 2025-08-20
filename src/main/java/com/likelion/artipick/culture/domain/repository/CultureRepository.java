1e017ac6e8f50f75310f4f93429bb7c82d02bf2e
quakwhad <quakwhad@gmail.com>
2025-08-21 02:23:50 +0900

    feat/#40: 검색 api 구현

--- src/main/java/com/likelion/artipick/culture/domain/repository/CultureRepository.java
+++ src/main/java/com/likelion/artipick/culture/domain/repository/CultureRepository.java
@@ -1,10 +1,13 @@
 package com.likelion.artipick.culture.domain.repository;
 
+import com.likelion.artipick.culture.domain.Category;
 import com.likelion.artipick.culture.domain.Culture;
 import com.likelion.artipick.user.domain.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -17,4 +20,13 @@ public interface CultureRepository extends JpaRepository<Culture, Long> {
     Page<Culture> findByUser(User user, Pageable pageable);
 
     Page<Culture> findByIsFromApi(Boolean isFromApi, Pageable pageable);
+
+    @Query("SELECT c FROM Culture c WHERE " +
+           "(:keyword IS NULL OR c.title LIKE %:keyword% OR c.contents LIKE %:keyword%) AND " +
+           "(:location IS NULL OR c.area LIKE %:location% OR c.sigungu LIKE %:location%) AND " +
+           "(:categoryEnum IS NULL OR c.category = :categoryEnum)")
+    Page<Culture> searchBy(@Param("keyword") String keyword,
+                           @Param("location") String location,
+                           @Param("categoryEnum") Category categoryEnum,
+                           Pageable pageable);
 }
